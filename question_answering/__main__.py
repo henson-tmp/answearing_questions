6036a6068d2dc2ea2f3d178995e008e658efc6c5
mstopa <m.stopa94@gmail.com>
2022-11-28 17:25:30 +0100

    Apply linter fromatting

--- question_answering/__main__.py
+++ question_answering/__main__.py
@@ -6,20 +6,25 @@ import spacy
 with open("../config.yaml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-QA_MODEL = pipeline("question-answering", model=config["model_name"], tokenizer=config["tokenizer"])
+QA_MODEL = pipeline(
+    "question-answering",
+    model=config["model_name"],
+    tokenizer=config["tokenizer"])
 POS_MODEL = spacy.load(config["lang_model"])
-
 wikipedia.set_lang(config["lang"])
+
 
 def get_wiki_text(name: str) -> str:
     prop_article = wikipedia.search(name)[0]
     try:
         article = wikipedia.page(prop_article)
     except wikipedia.exceptions.DisambiguationError as e:
-        article = wikipedia.page(e.options) #if there name may refer to more than one articles pick the first one
+        # if there name may refer to more than one articles pick the first one
+        article = wikipedia.page(e.options)
         print(e.options)
     print("Article title ", article.title)
     return article.content
+
 
 def get_subject(question: str) -> str:
     """Extracts a proper name from input text"""
@@ -34,7 +39,7 @@ def main():
         search = get_subject(question)
         context = get_wiki_text(search)
 
-        output = QA_MODEL({'context': context,'question': question})
+        output = QA_MODEL({'context': context, 'question': question})
         print(output)
 
 
